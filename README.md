14f301f2bd0172786d7a29239d45a7c3b9907a71
Seamus Foley <seamus.foley@icloud.com>
2020-05-12 10:18:09 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# `iotes/strategy-xxx`
+
+> TODO: description
+
+## Usage
+
+```
+import strategy from '@iotes/strategy-xxx'
+
+// TODO: DEMONSTRATE USAGE
+```
+
+## How to create a strategy 
+
+Documentation on how to create a strategy is [avaliable here](https://iotes.dev/docs/advanced/strategies)
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import mqtt, { MqttClient } from 'mqtt'
+import {
+  DeviceFactory,
+  HostConfig,
+  Strategy,
+  ClientConfig,
+  DeviceConfig,
+} from 'iotes'
+import { DeviceTypes, StrategyConfig } from './types'
+
+export const mqttStrategy: Strategy<StrategyConfig, DeviceTypes> = ({
+  hostDispatch,
+  deviceDispatch,
+  hostSubscribe,
+  deviceSubscribe,
+}) => async (
+  hostConfig: HostConfig<StrategyConfig>,
+  clientConfig: ClientConfig,
+): Promise<DeviceFactory<DeviceTypes>> => {
+  // HOST FACTORY
+  // Do host set up here
+
+  const deviceFactory = (): DeviceFactory<DeviceTypes> => {
+    // DEVICE FACTORIES
+
+    const createDevice = async (
+      deviceConfig: DeviceConfig<'DEVICE_TYPE'>,
+    ) => {
+      // Do device set up here
+      const deviceSetUp = () => {}
+
+      deviceSetUp()
+
+      return deviceConfig
+    }
+
+    // Return dictionary of device factories
+    return {
+      DEVICE_TYPE: createDevice,
+    }
+  }
+
+  return deviceFactory()
+}
+
+// Export types
+export { DeviceTypes, StrategyConfig }
